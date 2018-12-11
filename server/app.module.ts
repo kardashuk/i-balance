46eedda3ca0de107a945e4631ab7b6a7fff39b9b
Dmytro Kardashuk <dkardashuk@cogniance.com>
2018-12-11 17:35:40 +0200

    GraphQL instead of rest

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+      GraphQLModule.forRoot({
+          typePaths: ['./**/*.graphql'],
+      }),
+      CatsModule
+
+  ],
 })
 export class ApplicationModule {}
